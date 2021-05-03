82956dca71ba12ef05d800f3de75935d67048601
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2021-05-03 07:20:42 +0100

    program

---
+++ us_phone_number_format.py
@@ -0,0 +1,16 @@
+def phone(n):
+    num=[]
+    s=''
+
+    print("Enter numbers : ")
+    for i in range(n):
+        num.append(int(input()))
+
+    for i in range(n):
+        s += str(num[i])
+
+    return f"({s[0:3]}) {s[3:6]}-{s[6:-1]}
+
+
+print(phone(10))
+
